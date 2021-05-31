722663ef1d1672521caf9ea199f43b94894f519a
benjamingordon27 <benjamingordon27@gmail.com>
2021-05-31 16:54:37 -0400

    try flex on the tracker map page

--- containers/TrackerMapPage/TrackerMapPage.js
+++ containers/TrackerMapPage/TrackerMapPage.js
@@ -14,13 +14,13 @@ const TrackerMapPage = () => {
         <div>
             {/* <Controls /> */}            
             <div style={{display:'flex', flexDirection:'row'}}>
-                <div>
+                <div style={{width: '45%', flex: '1'}}>
                     <Sidebar />
                 </div>            
-                <div style={{position: 'relative',width: '100%', height:'100vh', right: '0',overflowY: 'scroll'}}>
+                <div style={{flex: '2',position: 'relative',width: '100%', height:'100vh', right: '0',overflowY: 'scroll'}}>
                     <TrackerMap />                                          
                 </div>            
-                <div className = 'legend' style={{position: 'relative',width: 'auto', height: '100vh', right: '0'}}>
+                <div className = 'legend' style={{position: 'relative',width: 'auto', height: '100vh', right: '0', flex: '1'}}>
                     <Legend legendMarkers = {mapMarkerImgs} zoom = {sidebarContext.zoom} filter={controlsContext.changeCategoryControls}/>
                 </div>            
             </div>
