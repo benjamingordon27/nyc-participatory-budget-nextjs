199dde9eed8c74f10550c1d3fe0f114f44383a81
benjamingordon27 <benjamingordon27@gmail.com>
2021-06-01 00:04:20 -0400

    begin to migrate navigation bar and controls into the same component

--- components/NavigationBar/NavigationBar.js
+++ components/NavigationBar/NavigationBar.js
@@ -2,7 +2,7 @@ import React, {useContext} from 'react';
 import classes from './NavigationBar.module.css';
 import Link from 'next/link';
 import SidebarContext from '../../store/dataContext/sidebarContext';
-import DistrictsDataContext from '../../store/dataContext/sidebarContext';
+import DistrictsDataContext from '../../store/dataContext/dataContext';
 import {uniqueValueBudget, uniqueValueCouncilMember} from '../../util/utility';
 import * as actions from '../../store/actions/actionTypes';
 import Controls from '../../containers/Controls/Controls';
@@ -12,7 +12,7 @@ const navigationBar = (props) => {
     const sidebarContext = useContext(SidebarContext);
     const districtsDataContext = useContext(DistrictsDataContext);
 
-    console.log(districtsDataContext.participatoryBudgetTracker);
+    console.log(districtsDataContext);
 
     return(
         <header className={classes.NavigationBar}>
@@ -23,7 +23,7 @@ const navigationBar = (props) => {
                         sidebarContext.dispatchSidebarData({type: actions.CLEAR_CLICKED_ITEM_REDUCER});
                     }}><Link href='/'>About</Link> | </li>                    
                     <li><Link href='/budget-item-tracker'>Budget Item Tracker</Link></li>                                        
-                        {districtsDataContext.participatoryBudgetTracker ? 
+                        {/* {districtsDataContext.participatoryBudgetTracker ? 
                             <li>
                                 <HoverDropdown 
                                     id = {'agencyHoverDropdown'}
@@ -32,7 +32,7 @@ const navigationBar = (props) => {
                                     clicked={(event) => {console.log(event.target.innerHTML); changeAgencyControls(event.target.innerHTML)}}
                                 />
                             </li>
-                        : <li>Agency |</li>}                        
+                        : <li>Agency |</li>}                         */}
                     <li>Project Status | </li>
                     <li>Category | </li>
                     <li>Year | </li>
