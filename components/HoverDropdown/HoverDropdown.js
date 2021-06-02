631e33756bc2a100665107e38b96384d4cb0896a
benjamingordon27 <benjamingordon27@gmail.com>
2021-06-02 16:14:55 -0400

    set the budget tracker page to also include controls and be updated

--- components/HoverDropdown/HoverDropdown.js
+++ components/HoverDropdown/HoverDropdown.js
@@ -4,7 +4,7 @@ import classes from './HoverDropdown.module.css';
 function HoverDropdown(props) {
     let options = '';
     if(props.list)
-        options = props.list.map(item => <div onClick={props.clicked} key={item} value={item}>{item}</div>);
+        options = props.list.map(item => <li onClick={props.clicked} key={item} value={item}>{item}</li>);
 
     return (
         <div className={classes.dropdown}>
--- pages/budget-item-tracker/index.js
+++ pages/budget-item-tracker/index.js
@@ -2,6 +2,7 @@ import {useRouter} from 'next/router';
 import React, {useContext, useState, useEffect} from 'react';
 import BudgetItemTrackerList from '../../containers/BudgetItemTrackerList/BudgetItemTrackerList';
 import DistrictDataContext from '../../store/dataContext/dataContext';
+import ControlsContext from '../../store/dataContext/controlsContext';
 import Spinner from '../../components/Spinner/Spinner';
 import SearchField from '../../components/SearchField/SearchField'
 import Controls from '../../containers/Controls/Controls';
@@ -11,6 +12,7 @@ const BudgetItemTrackerPage = () => {
     const router = useRouter();    
 
     const districtsDataContext = useContext(DistrictDataContext);            
+    const controlsContext = useContext(ControlsContext);      
 
     const [trackerList, setTrackerList] = useState([]);
     
@@ -18,7 +20,30 @@ const BudgetItemTrackerPage = () => {
         if(districtsDataContext.participatoryBudgetTracker){
             setTrackerList(districtsDataContext.participatoryBudgetTracker.features);
         }
-    }, [districtsDataContext.dataLoaded])
+    }, [districtsDataContext.dataLoaded]);
+
+    useEffect(() => {
+        if(districtsDataContext.dataLoaded){
+            console.log("controlsContext has changed", controlsContext);            
+            var outputList = districtsDataContext.participatoryBudgetTracker.features;
+            if(controlsContext.year !== ''){
+                outputList = outputList.filter(item => item.properties.vote_year === controlsContext.year);
+            }
+            if(controlsContext.category !== ''){
+                outputList = outputList.filter(item => item.properties.pinCategory === controlsContext.category);
+            }
+            if(controlsContext.status !== ''){
+                outputList = outputList.filter(item => item.properties.status_summary === controlsContext.status);
+            }
+            if(controlsContext.agency !== ''){
+                outputList = outputList.filter(item => item.properties.agency === controlsContext.agency);
+            }
+            if(controlsContext.districtsDataContext !== ''){
+                outputList = outputList.filter(item => item.properties.district === controlsContext.district.split(':')[0]);
+            }
+            setTrackerList(outputList);
+        }
+    }, [districtsDataContext.dataLoaded, controlsContext])
     
     const onChange = (event) => {
         if(districtsDataContext.participatoryBudgetTracker){
@@ -31,11 +56,14 @@ const BudgetItemTrackerPage = () => {
             console.log(trackerList);
             router.push(router.route + '?query='+event.toLowerCase())
         }
-    };    
+    };
 
     return (
         <div>            
             <h2 style={{padding: '12px'}}>Search for individual items below</h2>
+            <div>
+                <Controls />
+            </div>
             {districtsDataContext.dataLoaded ? 
                 <React.Fragment>
                     <SearchField placeholder='Search item' onChange={(event) => onChange(event.target.value)}/>
